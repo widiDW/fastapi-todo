e4318965470d6b911cc584fb83394496c0226135
unknown <michelmoniharapon@gmail.com>
2026-05-01 03:05:07 +0700

    fix: add update all schenas

--- app/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-# Ini biar bisa import: from app import schemas -> schemas.CourseResponse
-from.course import CourseCreate, CourseUpdate, CourseResponse
-from.user import UserCreate, UserResponse # Sesuaiin nama class di user.py lu
-from.pagination import PaginatedResponse # Sesuaiin nama class di pagination.py lu
