0ba98a7ca3e2efc91adf7d1136798b75995d0f16
unknown <michelmoniharapon@gmail.com>
2026-05-01 01:32:04 +0700

    fix: add updated_at to course neo model

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from .user import User
+from .course import Course
+from .enrollment import Enrollment
+from .module import Module
+from .lesson import Lesson
+
+__all__ = ["User", "Course", "Enrollment", "Module", "Lesson"]
