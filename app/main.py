33d8b2d011dc4ee5df28cb7b8c4d0b61a608083d
unknown <michelmoniharapon@gmail.com>
2026-05-01 01:57:06 +0700

    trigger main redeploy

--- app/main.py
+++ app/main.py
@@ -1,26 +1,22 @@
 from fastapi import FastAPI
-from app.database import Base, engine
-from app.routers import course, user, auth
+from app.database import engine, Base
+from app.routers.user import router as user_router
+from app.routers.course import router as course_router
+from app.auth import router as auth_router  # FIX DISINI
 from app.db.seed import run_seed
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(
-    title="Global Academy API",
-    swagger_ui_parameters={"persistAuthorization": True}
-)
+app = FastAPI()
 
 @app.on_event("startup")
-async def startup_event():
-    print("Running startup event...")
+def on_startup():
     run_seed()
-    print("Startup event done!")
 
-# Include router
-app.include_router(user.router, prefix="/api/v1", tags=["Users"])
-app.include_router(auth.router, prefix="/api/v1", tags=["Auth"])
-app.include_router(course.router, prefix="/api/v1", tags=["Courses"])
+app.include_router(user_router, prefix="/api/v1", tags=["Users"])
+app.include_router(course_router, prefix="/api/v1", tags=["Courses"])
+app.include_router(auth_router, prefix="/api/v1", tags=["Auth"])
 
 @app.get("/")
 def root():
-    return {"status": "Global Academy API udah online 🔥"}
+    return {"message": "Global Academy API"}
