1c1270a857f57375d29d5e3706bca22bc914bf33
unknown <michelmoniharapon@gmail.com>
2026-05-01 11:50:30 +0700

    fix: add dotenv

--- app/database.py
+++ app/database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
