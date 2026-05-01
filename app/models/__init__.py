1516b50b22ee71cef678ef0244b14be73dc87da6
unknown <michelmoniharapon@gmail.com>
2026-05-01 11:54:53 +0700

    fix: add chapter init models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,6 @@
+from .user import User
+from .course import Course
 from .chapter import Chapter, Lesson
-__all__ = ["User", "Course", "Chapter", "Lesson"]
+from .enrollment import Enrollment  # ← IMPORT TERAKHIR
+
+__all__ = ["User", "Course", "Chapter", "Lesson", "Enrollment"]
