f15e7030babddb22056143e7fa33bf474efd764e
unknown <michelmoniharapon@gmail.com>
2026-05-02 11:26:40 +0700

    fix: add prefix double user

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.database import engine, Base
-from app.routers.user import router as user_router
+#from app.routers.user import router as user_router
 from app.routers.course import router as course_router
 from app.auth import router as auth_router
 from app.db.seed import run_seed
@@ -14,11 +14,11 @@ app = FastAPI(title="Global Academy API")
 def on_startup():
     run_seed()
 
-app.include_router(user_router, prefix="/api/v1", tags=["Users"])
+#app.include_router(user_router, prefix="/api/v1", tags=["Users"])
 app.include_router(course_router, prefix="/api/v1", tags=["Courses"])
 app.include_router(auth_router, prefix="/api/v1", tags=["Auth"])
 
-#app.include_router(user.router, tags=["users"])
+app.include_router(user.router, prefix="/api/v1", tags=["users"])
 app.include_router(course.router, prefix="/api/v1", tags=["courses"])
 app.include_router(chapter.router, prefix="/api/v1", tags=["chapters"])
 app.include_router(enrollment.router, prefix="/api/v1", tags=["enrollments"])
