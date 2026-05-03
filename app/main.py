b5e3bba8894a863b00a736c1171485a84aea6603
unknown <michelmoniharapon@gmail.com>
2026-05-03 09:54:46 +0700

    fix: remove duplicate imports

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.database import engine, Base
 #from app.routers.user import router as user_router
-from app.routers.course import router as course_router
+#from app.routers.course import router as course_router
 from app.auth import router as auth_router
 from app.db.seed import run_seed
 from app.routers import user, course, chapter, enrollment
@@ -15,7 +15,7 @@ def on_startup():
     run_seed()
 
 #app.include_router(user_router, prefix="/api/v1", tags=["Users"])
-app.include_router(course_router, prefix="/api/v1", tags=["Courses"])
+#app.include_router(course_router, prefix="/api/v1", tags=["Courses"])
 app.include_router(auth_router, prefix="/api/v1", tags=["Auth"])
 
 app.include_router(user.router, prefix="/api/v1", tags=["users"])
